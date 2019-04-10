021063ed33eacd5ef6f6c90a44bfcd7c3d64d3e1
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2019-04-09 22:08:11 -0600

    working again

---
+++ client/build/precache-manifest.cade96c5c132bdc918bf0a5c8633d847.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6537a6c06d4762bebd6",
+    "url": "/static/js/main.c073fb58.chunk.js"
+  },
+  {
+    "revision": "ae0f2b73ae21b0738a7a",
+    "url": "/static/js/2.70cab3c4.chunk.js"
+  },
+  {
+    "revision": "d6537a6c06d4762bebd6",
+    "url": "/static/css/main.5c52ad9b.chunk.css"
+  },
+  {
+    "revision": "90e90b3b05b787fd39e0eebb045089e9",
+    "url": "/index.html"
+  }
+];
